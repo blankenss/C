9018170f9a77d1edc60897b66b2b637378950ee4
blankenss <2304658150@qq.com>
2025-11-17 09:35:26 +0800

    100-999之间的水仙花数
    Add program to find and print Narcissistic numbers.

---
+++ _6.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <math.h>
+int main(int argc, char const *argv[])
+{
+    int i;
+    for(i=100;i<=999;i++)
+    {
+        int a=i/100;
+        int b=i%100/10;
+        int c=i%10;
+        if(i==pow(a,3)+pow(b,3)+pow(c,3))
+        {
+            printf("%d是水仙花数\n",i);
+        }
+    }
+    return 0;
+}
